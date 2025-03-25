9c6f362ddde6c3162af0dcf933fcf659162830ad
Andri sigiro <sigiroandri@gmail.com>
2025-03-25 15:38:18 +0700

    olo

--- Backend Web/routes/api.php
+++ Backend Web/routes/api.php
@@ -18,5 +18,5 @@ use App\Http\Controllers\Admin\ResidentsController;
 
 Route::get('/products', [PostController::class, 'getProduct']);
 Route::get('/news', [PostController::class, 'getNews']);
-Route::get('penduduk', [PostController::class, 'getResidents']);
+Route::get('/penduduk', [ResidentsController::class, 'CheckNik']);
 Route::get('/pengumuman', [PostController::class, 'getAnnouncements']);
