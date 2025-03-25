50eca9b76fecfa6c7520cb6c91ef5bc08b94e116
Andri sigiro <sigiroandri@gmail.com>
2025-03-25 14:16:01 +0700

    residents

--- Backend Web/routes/api.php
+++ Backend Web/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ResidentsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/products', [PostController::class, 'getProduct']);
-Route::get('/news', [PostController::class, 'getNews']);
-Route::get('penduduk', [PostController::class, 'getResidents']);
+Route::get('/news', [PostController::class, 'getnews']);
+Route::get('/penduduk', [ResidentsController::class, 'CheckNik']);
